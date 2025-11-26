96697554484dccbd040258d7f8c75900f5b84166
JagpreetJakhar <jagpreetjakhar94@gmail.com>
2025-11-26 10:19:46 +0000

    Setting Up

--- src/eda/eda.py
+++ src/eda/eda.py
@@ -6,24 +6,7 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    from os.path import basename,exists
-    return basename, exists
-
-
-@app.cell
-def _(basename, exists):
-    def download(url):
-        filename = basename(url)
-        if not exists(filename):
-            from urllib.request import urlretrieve
-            local,_ = urlretrieve(url,filename)
-            print(f"Downloaded {local}")
-    return (download,)
-
-
-@app.cell
-def _(download):
-    download("https://github.com/AllenDowney/ThinkStats/raw/v3/nb/thinkstats.py")
+    ## Test
     return
 
 
